95ddc64e3de39f78fa2cef80fb548361321a2c43
Nikhil Devaraj <nikhil.nalluraj@gmail.com>
2020-08-05 15:03:29 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $("h3").click(function(){
+    $(this).hide();
+  });
+});
+
+$(document).ready(function(){
+  $("#button").click(function(){
+    $("#div1").fadeIn();
+  });
+});
